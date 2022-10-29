577c39b976055e9148130c5981d4105cb3d68dc5
Sweta Jain <swetajain04@gmail.com>
2022-10-29 17:34:34 +0530

    final

--- lib/src/presentation/views/main/main_page.dart
+++ lib/src/presentation/views/main/main_page.dart
@@ -1,5 +1,10 @@
+import 'package:deep_link_flutter/src/utils/constants.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -17,7 +22,7 @@ class _MainPageState extends State<MainPage> {
   @override
   void initState() {
     super.initState();
-    initDynamicLink();
+    // initDynamicLink();
   }
 
   Future<void> initDynamicLink() async {
@@ -26,13 +31,51 @@ class _MainPageState extends State<MainPage> {
       final queryParams = uri.queryParameters;
       if (queryParams.isNotEmpty) {
         String? productId = queryParams['id'];
-        Navigator.pushNamed(context, dynamicLinkData.link.path,
+        Navigator.restorablePushNamed(context, dynamicLinkData.link.path,
             arguments: {"productId": int.parse(productId!)});
+        print('...........');
+        print(dynamicLinkData.link.path);
       } else {
-        Navigator.pushNamed(context, dynamicLinkData.link.path);
+        Navigator.restorablePushNamed(context, dynamicLinkData.link.path);
+        print('...........');
+
+        print(dynamicLinkData.link.path);
       }
     }).onError((error) {
-      print(error);
+      if (kDebugMode) {
+        print('...........');
+
+        print(
+            '...........init dynamic link error.............................');
+        print('...........');
+
+        print(error);
+      }
+    });
+  }
+
+  Future<void> _createDynamicLink(bool short, String link) async {
+    setState(() {
+      _isCreatingLink = true;
+    });
+
+    final DynamicLinkParameters dynamicLinkParameters = DynamicLinkParameters(
+        link: Uri.parse(kUriPrefix + link),
+        uriPrefix: kUriPrefix,
+        androidParameters:
+            const AndroidParameters(packageName: kPackage, minimumVersion: 0));
+
+    Uri url;
+    if (short) {
+      final ShortDynamicLink shortDynamicLink =
+          await dynamicLinks.buildShortLink(dynamicLinkParameters);
+      url = shortDynamicLink.shortUrl;
+    } else {
+      url = await dynamicLinks.buildLink(dynamicLinkParameters);
+    }
+    setState(() {
+      _isCreatingLink = false;
+      _linkMessage = url.toString();
     });
   }
 
@@ -52,17 +95,76 @@ class _MainPageState extends State<MainPage> {
                     alignment: MainAxisAlignment.center,
                     children: [
                       ElevatedButton(
-                          onPressed: () {}, child: const Text("Get Long Link")),
+                          onPressed: !_isCreatingLink
+                              ? () => _createDynamicLink(false, kHomepageLink)
+                              : null,
+                          child: const Text("Get Long Link")),
                       ElevatedButton(
-                          onPressed: () {},
+                          onPressed: !_isCreatingLink
+                              ? () => _createDynamicLink(true, kHomepageLink)
+                              : null,
                           child: const Text("Get Short Link")),
                     ],
-                  )
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      if (_linkMessage != null) {
+                        await launch(_linkMessage!);
+                      }
+                    },
+                    onLongPress: () {
+                      Clipboard.setData(ClipboardData(text: _linkMessage));
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Link Copied")));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            borderRadius:
+                                BorderRadius.all(Radius.elliptical(40, 80)),
+                            color: Colors.yellowAccent.shade100),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(
+                            _linkMessage ?? "",
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                color: Colors.indigo,
+                                fontStyle: FontStyle.italic),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () => _onShare(context),
+                      child: const Text("Share Link")),
                 ],
               ),
             );
           },
         ) // This trailing comma makes auto-formatting nicer for build methods.
         );
+  }
+
+  void _onShare(BuildContext context) async {
+    // A builder is used to retrieve the context immediately
+    // surrounding the ElevatedButton.
+    //
+    // The context's `findRenderObject` returns the first
+    // RenderObject in its descendent tree when it's not
+    // a RenderObjectWidget. The ElevatedButton's RenderObject
+    // has its position and size after it's built.
+    final box = context.findRenderObject() as RenderBox?;
+    if (_linkMessage != null) {
+      await Share.share(_linkMessage!,
+          subject: 'Welcome home',
+          sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size);
+    } else {
+      // deep link generated from the console with text message and image
+      await Share.share("https://swetajain.page.link/home");
+    }
   }
 }
---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,4 @@
+const String kUriPrefix = 'https://swetajain.page.link';
+const String kHomepageLink = '/homepage';
+const String kProductpageLink = '/productpage?id=24';
+const String kPackage = 'com.swetajain.deep_link_flutter';
